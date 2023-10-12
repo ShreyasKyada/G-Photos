c3c1a9ab3778ea8378d43b7efef8e6d56df5d93f
ShreyasKyada <kyadashreyas@gmail.com>
2023-10-13 00:43:52 +0530

    added date picker filer

--- src/services/getAlbumItems.ts
+++ src/services/getAlbumItems.ts
@@ -1,20 +1,17 @@
 import { axiosInstance } from "@/axios/axios";
 import { BASE_URL } from "@/constants/constants";
 
-const getAlbumItems = async (albumId: string, params: any) => {
+const getAlbumItems = async (albumId: string, { dateRange, ...rest }: any) => {
   return axiosInstance.post(BASE_URL + "mediaItems:search", {
     albumId: albumId,
-    ...params,
-    // dateFilter: {
-    //   dates: [
-    //     {
-    //       year: 2023,
-    //       month: 10,
-    //       day: 3,
-    //     },
-    //   ],
-    // },
+    ...rest,
     // orderBy: "MediaMetadata.creation_time",
+    filters: {
+      dateFilter: {
+        ranges: [dateRange],
+      },
+      includeArchivedMedia: true,
+    },
   });
 };
 
